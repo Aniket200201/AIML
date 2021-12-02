5a3d63b2b671754ec5e95b50f93548f41244920f
Aniket200201 <73417528+Aniket200201@users.noreply.github.com>
2021-12-02 16:34:42 +0530

    Add files via upload

---
+++ exp6/q1.pl
@@ -0,0 +1,29 @@
+parent(smita, parag).
+parent(smita, rajeev).
+parent(ramesh, parag).
+parent(ramesh, rajeev).
+parent(parag, atharva).
+parent(shalaka, atharva).
+female(smita).
+female(shalaka).
+female(sayali).
+male(parag).
+male(atharva).
+male(ramesh).
+male(rajeev).
+mother(X, Y):- parent(X, Y), female(X).
+father(X, Y):- parent(X, Y), male(X).
+son(X, Y):- parent(Y, X), male(X).
+daughter(X, Y):- parent(Y, X), female(X).
+grandfather(X, Y):- parent(X, A), parent(A, Y), male(X).
+grandmother(X, Y):- parent(X, A), parent(A, Y), female(X).
+sister(X, Y):- parent(A, X), parent(A, Y), female(X), X \= Y.
+brother(X, Y):- parent(A, X), parent(A, Y), male(X), X \= Y.
+aunt(X, Y):- sister(X, Z), parent(Z, Y).
+uncle(X, Y):- brother(X, Z), parent(Z, Y).
+predecessor(X, Y) :- parent(X, Y).
+predecessor(X, Y) :- parent(X, A),predecessor(A, Y).
+successor(X, Y):- son(Y, X).
+successor(X, Y):- daughter(Y, X).
+successor(X, Y):- son(A, X), successor(A, Y).
+successor(X, Y):- daughter(A, X), successor(A, Y).
